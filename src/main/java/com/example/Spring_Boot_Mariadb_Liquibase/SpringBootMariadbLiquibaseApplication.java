b5db3f97c2c52f85df6525203d8b3085ed84981a
Sahiru22 <sahirubalasuriya222@gmail.com>
2025-08-13 02:01:08 +0530

    first commit

---
+++ src/main/java/com/example/Spring_Boot_Mariadb_Liquibase/SpringBootMariadbLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Boot_Mariadb_Liquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMariadbLiquibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMariadbLiquibaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Spring_Boot_Mariadb_Liquibase/SpringBootMariadbLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Boot_Mariadb_Liquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMariadbLiquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
